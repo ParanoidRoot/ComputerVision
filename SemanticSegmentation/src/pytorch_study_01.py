13b45ad3f6275bf899acdad109d0647d119e38de
ParanoidRoot <578791680@qq.com>
2019-10-30 23:26:35 +0800

    :memo: Update study note about pytorch & MNIST.

---
+++ SemanticSegmentation/src/pytorch_study_01.py
@@ -0,0 +1,198 @@
+# 用于学习 pytorch 下的深度学习的学习
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+
+class Utils(object):
+    '''工具类.'''
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    batch_size = 256
+
+    @classmethod
+    def print_tensor(cls, tensor):
+        print(tensor)
+        print(tensor.shape)
+
+    @classmethod
+    def print_with_type(cls, what):
+        '''打印内容, 并且显示类型.'''
+        print(what)
+        print(type(what))
+
+    @classmethod
+    def unnormalize(cls, img_tensor: torch.tensor, mean, std):
+        '''将一个图片还原回没有 normalize 之前的样子.'''
+        ans_tensor = torch.zeros_like(img_tensor)
+        for channel_index, channel_tensor in enumerate(img_tensor):
+            # 获得到每一个 channel 上的张量
+            ans_tensor[channel_index] = (
+                channel_tensor * std[channel_index] +
+                mean[channel_index]
+            )
+        return ans_tensor
+
+    @classmethod
+    def print_img_tensor(cls, img_tensor, label_title):
+        '''打印一张图片张量.
+        注意: 由于 plt 默认认为图片是 H * W * C
+        而数据集处理的图片是 C * H * W
+        '''
+        plt.figure()
+        plt.title(label_title)
+        plt.imshow(
+            img_tensor[0],
+            cmap='gray',  # 绘制黑白色
+            interpolation='none'  # 不适用插值算法
+        )
+        plt.show()
+
+
+class MNISTDataLoader(object):
+    '''MNIST 数据集的加载对象.'''
+    mean = (0.1307,)
+    std = (0.381,)
+
+    def get_data_tensors(self, data_set, need_shuffle=True):
+        '''将原始数据集转换成一个可以迭代的对象.'''
+        return torch.utils.data.DataLoader(
+            dataset=data_set, batch_size=Utils.batch_size,
+            shuffle=need_shuffle
+        )
+
+    def load_raw_data_set(
+        self, data_set_dir, need_download=True, is_train_set=True
+    ):
+        '''在这个方法中加载未处理的数据集.'''
+        # 数据集的图像预处理操作
+        transforms_list = torchvision.transforms.Compose(
+            [
+                # 转化一张图片变成 Channel * Height * Width
+                torchvision.transforms.ToTensor(),
+                # 将输入的 0 ~ 1 上的图片像素 (what / 255) 变成一个在一个区间范围内的分布数值
+                # 注意 MNIST 是单通道的, 所以只需要一个维度的元组即可
+                # 具体的数值是一个超参数, 经过训练可以确定优化的.
+                torchvision.transforms.Normalize(
+                    MNISTDataLoader.mean,
+                    MNISTDataLoader.std
+                )
+            ]
+        )
+        # 下载训练集, 返回一个 dataset 对象
+        data_set = torchvision.datasets.MNIST(
+            data_set_dir, train=is_train_set,  # 下载为训练集
+            download=need_download, transform=transforms_list
+        )
+        return data_set
+
+
+class MNISTLinearModel(nn.Module):
+    '''搭建一个特定网络结构的神经网络模型.'''
+    def __init__(self, _loss_function):
+        super(MNISTLinearModel, self).__init__()
+        # 配置网络层
+        self.h1 = nn.Linear(28 * 28, 256)
+        self.h2 = nn.Linear(256, 64)
+        self.h3 = nn.Linear(64, 10)
+        self.loss_function = _loss_function
+
+    def forward(self, input_x):
+        # 前向传播
+        h1_y = F.relu(self.h1(input_x))
+        h2_y = F.relu(self.h2(h1_y))
+        output_y = self.h3(h2_y)
+        return output_y
+
+    def train_a_batch(self, x_train, y_train):
+        '''
+        训练一个 batch 的数据.
+        0. 注意向量维度, 对于这个模型, 使用的时 [batch_size, features]
+        1. 前向传播一次 (可以直接用对象名, 使用 callable 方法, 也可以 forward 一次)
+        2. 计算损失值
+        3. 清空当前上一次的梯度值
+        4. 反向传播计算梯度
+        5. 使用优化器, 寻找一个下降的道路, 使得整个权重与偏置被更新
+        '''
+        # 1. 前向传播, 获取当前计算值
+        y_predict = self(x_train)
+        # 2. 计算损失值
+        loss = self.loss_function(y_predict, y_train)
+        loss_val = loss.item()
+        # 3. 清空上一次计算的梯度值 (Pytorch 中的 Variable 会保留梯度值)
+        # 因为是绑定的, 所以清空很重要, 清空后计算, 不然会使用上一次的信息
+        self.optimizer.zero_grad()
+        # 4. 反向传播, 计算梯度
+        loss.backward()
+        # 5. 优化器使用当前计算的梯度信息向前走一步
+        self.optimizer.step()
+        return loss_val
+
+    @classmethod
+    def trans_batch_data_tensor_2_one_tensor(
+        cls, batch_data_tensor: torch.Tensor
+    ):
+        '''将输入个一组图片, 拉成一组一条的 tensor, 然后合并成一个矩阵.'''
+        return batch_data_tensor.view((batch_data_tensor.shape[0], -1))
+
+    @classmethod
+    def trans_batch_labels_2_one_hot_tensor(
+        cls, batch_label_tensor, label_num
+    ):
+        '''将一个 tensor 转成一个 [batch_size, label_num].'''
+        ans_tensor = torch.zeros((batch_label_tensor.shape[0], label_num))
+        for index, label in enumerate(batch_label_tensor):
+            ans_tensor[index][label] = 1.0
+        return ans_tensor
+
+
+def main():
+    mnsit_data_loader = MNISTDataLoader()
+    # 训练集的训练
+    train_data_set = mnsit_data_loader.load_raw_data_set(
+        data_set_dir='./data/',
+        need_download=False, is_train_set=True
+    )
+    epoch_data_list = mnsit_data_loader.get_data_tensors(
+        data_set=train_data_set, need_shuffle=True
+    )
+    # 首先确定损失函数
+    loss_function = F.mse_loss
+    # 确定模型
+    model = MNISTLinearModel(loss_function)
+    model = model.to(Utils.device)
+    # 绑定并且添加优化器
+    optimizer = optim.SGD(model.parameters(), lr=0.005)
+    setattr(model, 'optimizer', optimizer)
+    # 开始训练 epoch
+    for epoch in range(3):
+        # print((' %d ' % epoch).center(31, '*'))
+        for batch_index, batch_data_with_labels_list in enumerate(
+            epoch_data_list
+        ):
+            # 获取一个 batch 的数据, 每个都是一个单通道的图片
+            batch_data_tensor = batch_data_with_labels_list[0]
+            x_train = MNISTLinearModel.trans_batch_data_tensor_2_one_tensor(
+                batch_data_tensor
+            )
+            x_train = x_train.to(Utils.device)
+            # 一个 tensor: [batch_size]
+            batch_label_tensor = batch_data_with_labels_list[1]
+            y_train = MNISTLinearModel.trans_batch_labels_2_one_hot_tensor(
+                batch_label_tensor, 10
+            )
+            y_train = y_train.to(Utils.device)
+            # 训练一个 batch
+            loss = model.train_a_batch(x_train, y_train)
+            print(
+                'epoch: %d, batch: %d, loss: %.4f' % (
+                    epoch, batch_index, loss
+                )
+            )
+    # 开始测试
+
+
+if __name__ == "__main__":
+    main()
---
+++ StudyMark.md
@@ -0,0 +1,171 @@
+# Study Mark
+
+
+
+## 1. Pytorch 的数据集加载与使用方式
+
+0. <font color='red' size=5>Pytorch 中采用的矩阵结构</font>
+    1. 对于输入 $x_{train}:~case * features$
+    2. 对于输出 $y_{train}:~case*labels$
+    3. 注意图像的输入 $img:~C*H*W$, 代表了 Channel * Height * Width
+
+1. 首先要加载原生的数据集
+
+    ```python
+    def load_raw_data_set(
+            self, data_set_dir, need_download=True, is_train_set=True
+        ):
+            '''在这个方法中加载未处理的数据集.'''
+            # 数据集的图像预处理操作
+            transforms_list = torchvision.transforms.Compose(
+                [
+                    # 转化一张图片变成 Channel * Height * Width
+                    torchvision.transforms.ToTensor(),
+                    # 将输入的 0 ~ 1 上的图片像素 (what / 255) 变成一个在一个区间范围内的分布数值
+                    # 注意 MNIST 是单通道的, 所以只需要一个维度的元组即可
+                    # 具体的数值是一个超参数, 经过训练可以确定优化的.
+                    torchvision.transforms.Normalize((0.1307,), (0.381,))
+                ]
+            )
+            # 下载训练集, 返回一个 dataset 对象
+            data_set = torchvision.datasets.MNIST(
+                data_set_dir, train=is_train_set,  # 下载为训练集
+                download=need_download, transform=transforms_list
+            )
+            return data_set
+    ```
+
+2. 然后使用 `torch.utils.data.DataLoader` 将原始数据集, 转化成 pytorch 中的 tensor 对象
+
+    ```python
+    def get_data_loader(self, data_set, need_shuffle=True):
+            '''将原始数据集转换成一个可以迭代的对象.'''
+            return torch.utils.data.DataLoader(
+                dataset=data_set, batch_size=Utils.batch_size,
+                shuffle=need_shuffle
+            )
+    ```
+
+3. **<font color='red'>迭代获取数据</font>**
+
+    ```python
+    	epoch_data_list = mnsit_data_loader.get_data_tensors(
+            data_set=data_set, need_shuffle=True
+        )
+        # Utils.print_with_type(data_loader)
+        for index, batch_data_list in enumerate(epoch_data_list):
+            # 每个迭代是一个列表
+            # 第一维是一个 batch 的数据的tensor, 每个元素是一个图片, shape: [batch_size, C, H, W]
+            batch_train_data_tensor = batch_data_list[0]
+            # 第二维是一个 batch 的标签的tensor, 每一个维度的值, shape: [batch_size,]
+            labels = batch_data_list[1]
+    ```
+
+
+
+
+
+## 2. Pytorch 的训练过程
+
+1. 第一步获取数据集
+    * 在目录 `1. Pytorch 的数据集加载和使用方式` 中已经阐明了
+2. 构建模型
+    1. 确定每层网络的结构
+    2. 确定 `forward` 方法中的每个 batch 的输入相应所做的运算, 包括激活函数等等, 返回一个结果向量, 要与 $y_{train}$ 的 $features$ 数目一样
+
+3. 确定损失函数 $loss\_fuction$
+
+4. 确定需要用到的 $optimizer$, 并且绑定到模型对象上
+
+    1. ```python
+        # 绑定并且添加优化器
+            optimizer = optim.SGD(model.parameters(), lr=0.005)
+            setattr(model, 'optimizer', optimizer)
+        ```
+
+    2. 
+
+5. 训练一个 batch 的过程
+
+    1. $forward$ 向前计算一次, 获取输出的 $y_{predict}$
+    2. 计算出当前的损失值 $loss = self.loss\_function(y_{predict}, y_{train})$
+        * 注意, 这个 $loss$ 中会保存动态的计算流图
+        * **<font color='red'>向前计算梯度时, 使用的是 $loss$ </font>**
+        * 获取损失值: `loss_val = loss.item()`
+
+    3. 清空上一次计算的梯度剩余值
+        * `self.optimizer.zero_grad()`
+
+    4. 向前传播, 计算梯度
+        * `loss.backward()`
+    5. 使用优化器, 更新当前的权重, 向着利用损失函数的方向移动一个 $lr$
+        * `self.optimizer.step()`
+
+    6. 综合全部
+
+        ```python
+        def train_a_batch(self, x_train, y_train):
+                '''
+                训练一个 batch 的数据.
+                0. 注意向量维度, 对于这个模型, 使用的时 [batch_size, features]
+                1. 前向传播一次 (可以直接用对象名, 使用 callable 方法, 也可以 forward 一次)
+                2. 计算损失值
+                3. 清空当前上一次的梯度值
+                4. 反向传播计算梯度
+                5. 使用优化器, 寻找一个下降的道路, 使得整个权重与偏置被更新
+                '''
+                # 1. 前向传播, 获取当前计算值
+                y_predict = self(x_train)
+                # 2. 计算损失值
+                loss = self.loss_function(y_predict, y_train)
+                loss_val = loss.item()
+                # 3. 清空上一次计算的梯度值 (Pytorch 中的 Variable 会保留梯度值)
+                # 因为是绑定的, 所以清空很重要, 清空后计算, 不然会使用上一次的信息
+                self.optimizer.zero_grad()
+                # 4. 反向传播, 计算梯度
+                loss.backward()
+                # 5. 优化器使用当前计算的梯度信息向前走一步
+                self.optimizer.step()
+                return loss_val
+        ```
+
+6. 每个 $epoch$
+
+    ```python
+     # 首先确定损失函数
+        loss_function = F.mse_loss
+        # 确定模型
+        model = MNISTLinearModel(loss_function)
+        model = model.to(Utils.device)
+        # 绑定并且添加优化器
+        optimizer = optim.SGD(model.parameters(), lr=0.005)
+        setattr(model, 'optimizer', optimizer)
+        # 开始训练 epoch
+        for epoch in range(3):
+            # print((' %d ' % epoch).center(31, '*'))
+            for batch_index, batch_data_with_labels_list in enumerate(
+                epoch_data_list
+            ):
+                # 获取一个 batch 的数据, 每个都是一个单通道的图片
+                batch_data_tensor = batch_data_with_labels_list[0]
+                x_train = MNISTLinearModel.trans_batch_data_tensor_2_one_tensor(
+                    batch_data_tensor
+                )
+                x_train = x_train.to(Utils.device)
+                # 一个 tensor: [batch_size]
+                batch_label_tensor = batch_data_with_labels_list[1]
+                y_train = MNISTLinearModel.trans_batch_labels_2_one_hot_tensor(
+                    batch_label_tensor, 10
+                )
+                y_train = y_train.to(Utils.device)
+                # 训练一个 batch
+                loss = model.train_a_batch(x_train, y_train)
+                print(
+                    'epoch: %d, batch: %d, loss: %.4f' % (
+                        epoch, batch_index, loss
+                    )
+                )
+    ```
+
+    
+
